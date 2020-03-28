7ca69c0cad70837c61f8471651cfbb4ec1ba541e
Tamas Galffy <ezittgtx@gmail.com>
2020-03-28 22:38:01 +0100

    feat: WebSocket Server and Routing

---
+++ services/wsrouter.js
@@ -0,0 +1,79 @@
+const handlers = {
+  connect: [],
+  open: [],
+  message: [],
+  close: []
+}
+
+let wsServer
+
+function _withJSONSend (ws) {
+  const rawSend = ws.send
+  ws.send = function send (data) {
+    rawSend.apply(ws, [JSON.stringify(data)])
+  }
+  return ws
+}
+
+function _pushHandler (type, handler) {
+  handlers[type].push(handler)
+}
+
+function _callHandlers (type, ...args) {
+  handlers[type].forEach(handler => handler(...args))
+}
+
+function callConnect (ws) {
+  ws = _withJSONSend(ws)
+  _callHandlers('connect', ws)
+}
+
+function callOpen (ws) {
+  _callHandlers('open', ws)
+}
+
+function callMessage (ws, data) {
+  _callHandlers('message', ws, JSON.parse(data))
+}
+
+function callClose (ws, code, reason) {
+  _callHandlers('close', ws, code, reason)
+}
+
+function onConnect (handler) {
+  _pushHandler('connect', handler)
+}
+
+function onMessage (handler) {
+  _pushHandler('message', handler)
+}
+
+function onClose (handler) {
+  _pushHandler('close', handler)
+}
+
+/**
+ * Get or set WS Server
+ * @param {ws.Server} value WS Server
+ * @returns {ws.Server} server
+ */
+function server (value) {
+  if (value) {
+    wsServer = value
+  }
+
+  return wsServer
+}
+
+module.exports = {
+  callConnect,
+  callOpen,
+  callMessage,
+  callClose,
+
+  onConnect,
+  onMessage,
+  onClose,
+
+  server
+}
